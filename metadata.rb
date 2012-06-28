6ae02c98ca2da98534a98c51358e97c83a4e0cf4
Ben Browning <bbrownin@redhat.com>
2012-06-28 15:41:00 -0400

    Update maintainer to myself

--- metadata.rb
+++ metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Kevin Olbrich"
-maintainer_email "kolbrich@6fusion.com"
+maintainer       "Ben Browning"
+maintainer_email "ben324@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures torquebox"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
